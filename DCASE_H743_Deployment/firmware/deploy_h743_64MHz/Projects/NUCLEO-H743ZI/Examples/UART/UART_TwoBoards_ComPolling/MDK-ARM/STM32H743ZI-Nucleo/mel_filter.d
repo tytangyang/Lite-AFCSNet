e2c554abec6d1b5e6a7f40160075ab0eaa13711c
tytangyang <tytangyang66@gmail.com>
2026-09-01 17:17:21 +0800

    Add files via upload

---
+++ DCASE_H743_Deployment/firmware/deploy_h743_64MHz/Projects/NUCLEO-H743ZI/Examples/UART/UART_TwoBoards_ComPolling/MDK-ARM/STM32H743ZI-Nucleo/mel_filter.d
@@ -0,0 +1,6 @@
+stm32h743zi-nucleo\mel_filter.o: ../Dsp/mel_filter.c
+stm32h743zi-nucleo\mel_filter.o: ../Dsp/mel_filter.h
+stm32h743zi-nucleo\mel_filter.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdint.h
+stm32h743zi-nucleo\mel_filter.o: D:\keil5\ARM\ARMCC\Bin\..\include\math.h
+stm32h743zi-nucleo\mel_filter.o: D:\keil5\ARM\ARMCC\Bin\..\include\string.h
+stm32h743zi-nucleo\mel_filter.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdio.h
