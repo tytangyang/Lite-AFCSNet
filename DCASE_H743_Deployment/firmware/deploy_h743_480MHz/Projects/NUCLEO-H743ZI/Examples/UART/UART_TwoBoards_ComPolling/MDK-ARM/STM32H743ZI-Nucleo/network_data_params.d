d88717e43ae9d65451cb2654953f324fe0173366
tytangyang <tytangyang66@gmail.com>
2026-09-02 10:53:08 +0800

    Add files via upload

---
+++ DCASE_H743_Deployment/firmware/deploy_h743_480MHz/Projects/NUCLEO-H743ZI/Examples/UART/UART_TwoBoards_ComPolling/MDK-ARM/STM32H743ZI-Nucleo/network_data_params.d
@@ -0,0 +1,7 @@
+stm32h743zi-nucleo\network_data_params.o: ../Model/network_data_params.c
+stm32h743zi-nucleo\network_data_params.o: ../Model/network_data_params.h
+stm32h743zi-nucleo\network_data_params.o: ../AI/ai_platform.h
+stm32h743zi-nucleo\network_data_params.o: E:\Keil Core\ARM\ARMCC\Bin\..\include\stdint.h
+stm32h743zi-nucleo\network_data_params.o: E:\Keil Core\ARM\ARMCC\Bin\..\include\stddef.h
+stm32h743zi-nucleo\network_data_params.o: E:\Keil Core\ARM\ARMCC\Bin\..\include\inttypes.h
+stm32h743zi-nucleo\network_data_params.o: E:\Keil Core\ARM\ARMCC\Bin\..\include\stdbool.h
